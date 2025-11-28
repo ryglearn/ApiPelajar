48c2dc5c7e43defe3c21279b3ae3bcf325a4ff59
ryglearn <raffyoga99@gmail.com>
2025-11-28 08:12:25 +0700

    Update: Adding Cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const pelajarRoute = require('./routes/pelajarRoute')
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 // prefix
